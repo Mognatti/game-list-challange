5d569f6fc2d1821062025d5823e5b6c334703eb9
Caio Mognatti <caiocsm97@gmail.com>
2023-06-22 04:11:45 -0300

    feat: improve and organize the Card component for it to show the mandatory data with minor style implementation

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,17 +1,12 @@
-export type Game = {
-  developer: string;
-  freetogame_profile_url: string;
-  game_url: string;
-  genre: string;
-  id: number;
-  platform: string;
-  publisher: string;
-  release_date: string;
-  short_description: string;
-  thumbnail: string;
-  title: string;
-};
+import * as S from "../../styles/StyledComponents";
+import { Game } from "../../types";
 
-export default function Card(games: Game[]) {
-  return <div></div>;
+export default function Card(game: Game) {
+  return (
+    <S.CardContainer>
+      <S.CardThumb src={game.thumbnail} alt={`Imagem do jogo ${game.title}`} />
+      <S.CardTitle>{game.title}</S.CardTitle>
+      <S.CardGameGenre>Gênero: {game.genre}</S.CardGameGenre>
+    </S.CardContainer>
+  );
 }
