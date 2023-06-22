011ad9e30eb106d1a1ae86c6af7f9ec62f42c069
Caio Mognatti <caiocsm97@gmail.com>
2023-06-22 04:15:41 -0300

    feat: created new styled components and improve code organization

--- src/styles/StyledComponents.tsx
+++ src/styles/StyledComponents.tsx
@@ -1,5 +1,11 @@
 import { styled } from "styled-components";
 
+const paleta = {
+  cardBg: "rgba(0,0,0,0.7)",
+  cardFontColor: "#FFF",
+};
+
+// GameList
 export const GameList = styled.div`
   display: grid;
   grid-template-columns: repeat(auto-fit, minmax(400px, 1fr));
@@ -7,10 +13,25 @@ export const GameList = styled.div`
   list-style: none;
 `;
 
-export const DrawerDiv = styled.div`
-  background-color: rgba(0, 0, 0, 0.3);
+// Card
+export const CardContainer = styled.div`
+  background-color: ${paleta.cardBg};
+  border-radius: 15px;
+  display: flex;
+  flex-direction: column;
+  padding: 16px 0px;
+  align-items: center;
 `;
 
-export const GameItem = styled.li`
-  padding: 16px;
+export const CardThumb = styled.img`
+  border-radius: 15px;
+`;
+
+export const CardTitle = styled.h3`
+  color: ${paleta.cardFontColor};
+`;
+
+export const CardGameGenre = styled.p`
+  color: ${paleta.cardFontColor};
+  margin: 0;
 `;
