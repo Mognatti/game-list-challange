208c6b7e9d5d7ccb83a1b45cea26ef274925f012
Caio Mognatti <caiocsm97@gmail.com>
2023-06-23 14:18:44 -0300

    fix: minor change on code logic to fix a possible break

--- src/components/GameList/index.tsx
+++ src/components/GameList/index.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useFetch } from "../../hooks/useGetData";
+import { useFetch } from "../../hooks/useFetch";
 import * as S from "../../styles/StyledComponents";
 import { Game } from "../../types";
 import Card from "../Card";
@@ -14,34 +14,34 @@ export default function GameList() {
   );
 
   if (isError) return <p>{errorMessage}</p>;
-  if (isLoading) return <p>Carreagndo dados...</p>;
-
-  return (
-    <section>
-      <S.SearchContainer>
-        <p>Filtro por gênero</p>
-        <ul>
-          {genreList.map((genre: string) => (
-            <li key={genre}>
-              <button>{genre}</button>
-            </li>
-          ))}
-        </ul>
-        <S.TextBox>
-          <S.SearchInput
-            type="text"
-            placeholder="Busca por nome..."
-            value={search}
-            onChange={(e) => setSearch(e.target.value)}
-          />
-          <S.SearchIcon />
-        </S.TextBox>
-      </S.SearchContainer>
-      <S.GameList>
-        {filteredArray?.map((game: Game) => {
-          return <Card key={game.id} {...game} />;
-        })}
-      </S.GameList>
-    </section>
-  );
+  if (isLoading) return <p>Carregando dados...</p>;
+  if (gameList && genreList)
+    return (
+      <section>
+        <S.SearchContainer>
+          <p>Filtro por gênero</p>
+          <ul>
+            {genreList.map((genre: string) => (
+              <li key={genre}>
+                <button>{genre}</button>
+              </li>
+            ))}
+          </ul>
+          <S.TextBox>
+            <S.SearchInput
+              type="text"
+              placeholder="Busca por nome..."
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+            />
+            <S.SearchIcon />
+          </S.TextBox>
+        </S.SearchContainer>
+        <S.GameList>
+          {filteredArray?.map((game: Game) => {
+            return <Card key={game.id} {...game} />;
+          })}
+        </S.GameList>
+      </section>
+    );
 }
