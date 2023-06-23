8caaf63832692f54da1f6116d3292df16b08ebee
Caio Mognatti <caiocsm97@gmail.com>
2023-06-23 01:02:50 -0300

    feat: changed the styles and created more components to improve the aplication

--- src/styles/StyledComponents.tsx
+++ src/styles/StyledComponents.tsx
@@ -1,4 +1,5 @@
 import { styled } from "styled-components";
+import { BsSearch } from "react-icons/bs";
 
 const paleta = {
   cardBg: "rgba(0,0,0,0.7)",
@@ -6,6 +7,33 @@ const paleta = {
 };
 
 // GameList
+export const SearchContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+export const SearchInput = styled.input`
+  width: 100%;
+  border: none;
+  :focus-visible,
+  :focus {
+    outline-offset: none;
+    outline: none;
+    border: none;
+  }
+`;
+export const SearchIcon = styled(BsSearch)``;
+
+export const TextBox = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  padding-bottom: 8px;
+  width: 400px;
+  border-bottom: 1px solid black;
+`;
+
 export const GameList = styled.div`
   display: grid;
   grid-template-columns: repeat(auto-fit, minmax(400px, 1fr));
@@ -20,6 +48,7 @@ export const CardContainer = styled.div`
   display: flex;
   flex-direction: column;
   padding: 16px 0px;
+  margin: 8px 0;
   align-items: center;
 `;
 
