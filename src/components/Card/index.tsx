308e9dc1cadec1cd65a64510508e65b9a6d9b24a
Caio Mognatti <caiocsm97@gmail.com>
2023-06-24 01:14:47 -0300

    feat: change Card layout

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -3,6 +3,14 @@ import dayjs from "dayjs";
 import { Game } from "../../types";
 
 export default function Card(game: Game) {
+  const meContrata = [
+    `data de lançamento ${dayjs(game.release_date).format("DD/MM/YYYY")}`,
+    `Gênero ${game.genre}`,
+    `Produtora: ${game.developer}`,
+    `Plataformas: ${game.platform}`,
+    `Publisher: ${game.publisher}`,
+  ];
+
   return (
     <S.CardContainer>
       <S.CardContainerLink href={game.freetogame_profile_url} target="_blank">
@@ -12,22 +20,10 @@ export default function Card(game: Game) {
         />
         <S.CardTitle>{game.title}</S.CardTitle>
         <S.CardGameInfoContainer>
-          <S.CardGameInfo>
-            Data de lançamento: {dayjs(game.release_date).format("DD/MM/YYYY")}
-          </S.CardGameInfo>
-          <S.CardGameInfo>Gênero: {game.genre}</S.CardGameInfo>
-          <S.CardGameInfo>Produtora: {game.developer}</S.CardGameInfo>
-          <S.CardGameInfo>Plataformas: {game.platform}</S.CardGameInfo>
-          <S.CardGameInfo>Publisher: {game.publisher}</S.CardGameInfo>
+          {meContrata.map((contrata) => (
+            <S.CardGameInfo key={contrata}>{contrata}</S.CardGameInfo>
+          ))}
         </S.CardGameInfoContainer>
-        <S.CardGameInfoMoreContainer>
-          <S.CardGameInfo>Página da loja: </S.CardGameInfo>
-          <span>
-            <S.CardTextLink href={game.game_url} target="_blank">
-              {game.game_url}
-            </S.CardTextLink>
-          </span>
-        </S.CardGameInfoMoreContainer>
       </S.CardContainerLink>
     </S.CardContainer>
   );
