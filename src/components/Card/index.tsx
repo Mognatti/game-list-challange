00449c9973cc05f43f8f3f096cda78ed27215828
Caio Mognatti <caiocsm97@gmail.com>
2023-06-23 23:52:41 -0300

    feat: changes the way the card will be created to improve UX

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,12 +1,34 @@
 import * as S from "../../styles/StyledComponents";
+import dayjs from "dayjs";
 import { Game } from "../../types";
 
 export default function Card(game: Game) {
   return (
     <S.CardContainer>
-      <S.CardThumb src={game.thumbnail} alt={`Imagem do jogo ${game.title}`} />
-      <S.CardTitle>{game.title}</S.CardTitle>
-      <S.CardGameGenre>Gênero: {game.genre}</S.CardGameGenre>
+      <S.CardContainerLink href={game.freetogame_profile_url} target="_blank">
+        <S.CardThumb
+          src={game.thumbnail}
+          alt={`Imagem do jogo ${game.title}`}
+        />
+        <S.CardTitle>{game.title}</S.CardTitle>
+        <S.CardGameInfoContainer>
+          <S.CardGameInfo>
+            Data de lançamento: {dayjs(game.release_date).format("DD/MM/YYYY")}
+          </S.CardGameInfo>
+          <S.CardGameInfo>Gênero: {game.genre}</S.CardGameInfo>
+          <S.CardGameInfo>Produtora: {game.developer}</S.CardGameInfo>
+          <S.CardGameInfo>Plataformas: {game.platform}</S.CardGameInfo>
+          <S.CardGameInfo>Publisher: {game.publisher}</S.CardGameInfo>
+        </S.CardGameInfoContainer>
+        <S.CardGameInfoMoreContainer>
+          <S.CardGameInfo>Página da loja: </S.CardGameInfo>
+          <span>
+            <S.CardTextLink href={game.game_url} target="_blank">
+              {game.game_url}
+            </S.CardTextLink>
+          </span>
+        </S.CardGameInfoMoreContainer>
+      </S.CardContainerLink>
     </S.CardContainer>
   );
 }
