8da37716a6fd6f01c264731234bba5f7ab1ef4ec
Caio Mognatti <caiocsm97@gmail.com>
2023-06-24 01:25:03 -0300

    feat: improved code

---
+++ src/components/Games/index.tsx
@@ -0,0 +1,41 @@
+import { Game } from "../../types";
+import { useState, useEffect } from "react";
+import * as S from "../../styles/StyledComponents";
+import Card from "../Card";
+
+interface Props {
+  search: string;
+  filter: string | null;
+  gameList: Game[];
+}
+
+export default function Games({ search, filter, gameList }: Props) {
+  const [filteredList, setFilteredList] = useState<Game[]>();
+
+  const searchGame = (gameTitle: string) => {
+    const reg = new RegExp(search, "i");
+    return reg.test(gameTitle);
+  };
+
+  const filterGame = (gameGenre: string) => {
+    if (filter !== null) {
+      return filter === gameGenre;
+    }
+    return true;
+  };
+
+  useEffect(() => {
+    const newGameList = gameList.filter(
+      (game) => searchGame(game.title) && filterGame(game.genre)
+    );
+    setFilteredList(newGameList);
+  }, [search, filter]);
+
+  return (
+    <S.GameList>
+      {filteredList?.map((game: Game) => {
+        return <Card key={game.id} {...game} />;
+      })}
+    </S.GameList>
+  );
+}
